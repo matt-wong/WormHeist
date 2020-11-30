08b3a8ea38501f3a9059218c4d1d90c7b9fddfc5
Unknown <matthew.alexander.wong@gmail.com>
2020-11-29 19:45:12 -0700

    Load Project into Git.

---
+++ Assets/cameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+
+    public Transform player;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Debug.Log(transform.position);
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, -5); // Camera follows the player with specified offset position
+    }
+}
---
+++ Assets/playerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)){
+            this.MoveTowardMouse();
+            InvokeRepeating("MoveTowardMouse", 0.25f, 0.25f);
+        }else if (Input.GetKeyUp(KeyCode.Mouse0)){
+            CancelInvoke();
+        }
+    }
+
+    private void MoveTowardMouse(){
+        Rigidbody2D rb = GetComponentInChildren<Rigidbody2D>();
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = new Vector2(worldPosition.x - transform.position.x, worldPosition.y - transform.position.y);
+        
+        // float rot_z = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //  transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+
+        rb.AddForce(direction.normalized * 8000);
+    }
+}
