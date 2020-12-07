026fb156b131950837b86d817ec41f0dbba987ea
Unknown <matthew.alexander.wong@gmail.com>
2020-12-07 14:12:27 -0700

    Reload scene on death.

--- Assets/wormSegment.cs
+++ Assets/wormSegment.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class wormSegment : MonoBehaviour
 {
+    public bool IsVital = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,15 +16,20 @@ public class wormSegment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    
+
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.tag == "bullet" ){
-            Debug.Log("HIT");
-            Destroy(gameObject);
+        if (col.tag == "bullet")
+        {
+            if (IsVital)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }else{
+                Destroy(gameObject);
+            }
         }
     }
 }
